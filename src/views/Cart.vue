2e5427c98a00b60b1cd74854ff067039c1d3788d
masaokaka <osushiiisensu@gmail.com>
2021-05-10 20:09:50 +0900

    商品データをストアに追加+画像ファイルをassetフォルダに追加

---
+++ src/views/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+<v-container>
+    <img :src="items[1].img">
+</v-container>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            headers:[
+                {name:'商品名'},
+                {price:'価格'},
+            ],
+            items:this.$store.state.itemData
+        }
+    }
+}
+</script>
