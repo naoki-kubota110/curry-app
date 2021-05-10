925a98735131bc844e71e34c39d6fdfa6a3ad3e3
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-05-10 20:19:36 +0900

    ログイン処理を追加しました。

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,10 +6,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    login_user:null,
   },
   mutations: {
+    setLoginUser(state, user){
+      state.login_user = user;
+    },
   },
   actions: {
+    //ユーザー登録
+    register({state, commit}, {name,email,password}){
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        let user = firebase.auth().currentUser;
+        commit('setLoginUser', user);
+      }).catch((error) => {
+        state.errorMsg = error.message;
+      })
+    },
+    //ログイン
+    login({state,commit}, {email, password}){
+      firebase.auth().signInWithEmailAndPassword(email,password)
+      .then((user) => {
+        commit('setLoginUser', user);
+      }).catch((error) =>{
+        state.errorMsg = error.message;
+      })
+    },
+    errorDelete({commit}){
+      commit('errorDelete');
+    }
   },
   modules: {
   }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,1 +1,52 @@
+<template>
+    <v-app>
+        <v-row>
+            <v-col cols="12" sm="10" md="8" lg="6">
+                <v-card>
+                    <p v-if="$store.state.errorMsg">
+                        <strong>{{$store.state.errorMsg}}</strong>
+                    </p>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field v-model="email"
+                                        prepend-inner-icon="mdi-email" 
+                                        type="email" 
+                                        label="メールアドレス"/>
+                            <v-text-field v-model="password"
+                                        prepend-inner-icon="mdi-key"
+                                        type="password"
+                                        label="パスワード"/>
+                            <!-- ここ怪しい -->
+                            <v-btn @click="$router.push('/')">キャンセル</v-btn>
+                            <v-btn color="primary" @click="signIn">ログイン</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-app>
+</template>
 
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+    data(){
+        return {
+            email:'',
+            password:'',
+        }
+    },
+    created(){
+        this.errorDelete();
+    },
+    methods:{
+        ...mapActions(['login', errorDelete]),
+        signIn(){
+            this.login({email:this.email,password:this.password});
+            this.email = '';
+            this.password = '';
+        }
+    }
+}
+</script>
