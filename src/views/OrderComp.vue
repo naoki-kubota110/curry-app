09211310e7868c6aca73fb6d5f093d5d4cccd79c
“Toda-Atsuko” <atsuko.toda@rakus-partners.co.jp>
2021-05-13 10:51:12 +0900

    変更保存

---
+++ src/views/OrderComp.vue
@@ -0,0 +1,16 @@
+<template>
+<v-row>
+<v-col cols=12 lg=12 md=12 sm=12 align="center">
+  <h1>決済が完了しました！</h1>
+  <p>この度はご注文ありがとうございます。</p>
+  <p>お支払い先は、お送りしたメールに記載してありますのでご確認ください。</p>
+  <p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+  <v-btn
+      depressed
+      color="red"
+    >
+      トップ画面を表示する
+    </v-btn>
+</v-col>
+</v-row>
+</template>
