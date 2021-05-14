dc5d4fbecb1335ff6ff9bbc65e229310fd2c8668
“Toda-Atsuko” <atsuko.toda@rakus-partners.co.jp>
2021-05-14 10:05:58 +0900

    ヘッダーを変更しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,41 +1,35 @@
 <template>
-  <v-footer
-    color="#FFAB40"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="brown"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col
-        class=" py-4 text-center white--text"
-        cols="12"
-      >
-       Copyright©︎ {{ new Date().getFullYear() }} — <strong>ラクラクカリー All Rights Reserved</strong>
+  <v-footer color="#FFAB40" padless height="100px" dark>
+      <v-row justify="center" no-gutters >
+        <v-col align="center" cols="3">
+          <router-link to="/" class="ma-5 link" ><v-icon>mdi-home</v-icon></router-link>
+        </v-col>
+        <v-col align="center" cols="3">
+          <router-link style="" to="/cart" class="ma-5 link"
+            ><v-icon>mdi-cart</v-icon></router-link
+          >
+        </v-col>
+        <v-col align="center" cols="3" v-if="$store.state.login_user">
+          <router-link to="/orderhistory" class="ma-5 link" 
+            >注文履歴</router-link
+          >
+        </v-col>
+      </v-row>
+
+
+      <v-col class=" py-4 text-center white--text" cols="12">
+        Copyright©︎ {{ new Date().getFullYear() }} —
+        <strong>ラクラクカリー All Rights Reserved</strong>
       </v-col>
-    </v-row>
+
+  
+
   </v-footer>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      links: [
-        'ショッピングカート',
-        '注文履歴',
-        'ログアウト',
-      ],
-    }),
-  }
-</script>
-    
+<style scoped>
+.link {
+  text-decoration: none;
+  color: black;
+}
+</style>
